83cdc8d11928e6b9f2b9a2554b40188a9a649ac7
Zhonglu <wangzhonglu@gmail.com>
2022-01-30 22:33:20 -0600

    Add tests

--- test/expected/root/level1/level2/template.js
+++ test/expected/root/level1/level2/template.js
@@ -5,5 +5,7 @@ found: value
 array: element1,additionalValue
 overwritten: overwritten
 notFound: undefined
+templating: {param.nestedParam1},{param.nestedParam1}},undefined},undefined}
+templating2: undefined
 
 one more line...
--- test/input/root/level1/template.js.pp
+++ test/input/root/level1/template.js.pp
@@ -5,5 +5,8 @@ found: {{ value }}
 array: {{ array[0] }},{{ array[2] }}
 overwritten: {{ array[1] }}
 notFound: {{ array[3] }}
+templating: {param.nestedParam1},{param.nestedParam1}},{{{param.nestedParam1}}},{{{notFound}}}
+templating2: {{param.nestedParam1
+}}
 
 one more line...
